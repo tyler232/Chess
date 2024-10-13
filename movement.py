f61aaf96f0023f47bf93ce9175ca7f8864634795
Tyler Liu <cliu232@illinois.edu>
2024-10-12 22:47:26 -0500

    black basic movement added

--- movement.py
+++ movement.py
@@ -6,20 +6,33 @@ def move_piece(board, possible_moves, selected_piece, end_pos):
 
     piece = board[start_row][start_col]
     # Castling
-    if is_valid_move(possible_moves, end_pos) and can_castle(board, selected_piece, end_pos) == 1:
+    if piece == "wk" and is_valid_move(possible_moves, end_pos) and can_castle(board, selected_piece, end_pos) == 1:
         board[start_row][start_col] = ""
         board[end_row][end_col] = piece
         board[7][7] = ""
         board[7][5] = "wr"
-    elif is_valid_move(possible_moves, end_pos) and can_castle(board, selected_piece, end_pos) == 2:
+    elif piece == "wk" and is_valid_move(possible_moves, end_pos) and can_castle(board, selected_piece, end_pos) == 2:
         board[start_row][start_col] = ""
         board[end_row][end_col] = piece
         board[7][0] = ""
         board[7][3] = "wr"
+    elif piece == "bk" and is_valid_move(possible_moves, end_pos) and can_castle(board, selected_piece, end_pos) == 1:
+        board[start_row][start_col] = ""
+        board[end_row][end_col] = piece
+        board[0][7] = ""
+        board[0][5] = "br"
+    elif piece == "bk" and is_valid_move(possible_moves, end_pos) and can_castle(board, selected_piece, end_pos) == 2:
+        board[start_row][start_col] = ""
+        board[end_row][end_col] = piece
+        board[0][0] = ""
+        board[0][3] = "br"
     # Queening
     elif is_valid_move(possible_moves, end_pos) and piece == "wp" and end_row == 0:
         board[start_row][start_col] = ""
         board[end_row][end_col] = "wq"
+    elif is_valid_move(possible_moves, end_pos) and piece == "bp" and end_row == 7:
+        board[start_row][start_col] = ""
+        board[end_row][end_col] = "bq"
     # Regular move
     elif is_valid_move(possible_moves, end_pos):
         board[start_row][start_col] = ""
@@ -33,11 +46,18 @@ def can_castle(board, start_pos, end_pos):
     piece = board[start_row][start_col]
     if piece[1] != "k":
         return 0
-
-    if start_row == 7 and start_col == 4 and end_row == 7 and end_col == 6:
-        return 1
-    elif start_row == 7 and start_col == 4 and end_row == 7 and end_col == 2:
-        return 2
+    
+    if piece[0] == "w":
+        if start_row == 7 and start_col == 4 and end_row == 7 and end_col == 6:
+            return 1
+        elif start_row == 7 and start_col == 4 and end_row == 7 and end_col == 2:
+            return 2
+    elif piece[0] == "b":
+        if start_row == 0 and start_col == 4 and end_row == 0 and end_col == 6:
+            return 1
+        elif start_row == 0 and start_col == 4 and end_row == 0 and end_col == 2:
+            return 2
+        
     return 0
 
 def is_valid_move(possible_moves, end_pos):
@@ -45,87 +65,100 @@ def is_valid_move(possible_moves, end_pos):
 
 def get_possible_moves(board, selected_piece):
     piece = board[selected_piece[0]][selected_piece[1]]
-    if piece == "wp":
-        return possible_moves_pawn(board, selected_piece)
-    elif piece == "wn":
-        return possible_moves_knight(board, selected_piece)
-    elif piece == "wb":
-        return possible_moves_bishop(board, selected_piece)
-    elif piece == "wr":
-        return possible_moves_rook(board, selected_piece)
-    elif piece == "wq":
-        return possible_moves_bishop(board, selected_piece) + possible_moves_rook(board, selected_piece)
-    elif piece == "wk":
-        return possible_moves_king(board, selected_piece)
+    if not piece:
+        return []
+    
+    if piece[1] == "p":
+        return possible_moves_pawn(board, selected_piece, piece[0])
+    elif piece[1] == "n":
+        return possible_moves_knight(board, selected_piece, piece[0])
+    elif piece[1] == "b":
+        return possible_moves_bishop(board, selected_piece, piece[0])
+    elif piece[1] == "r":
+        return possible_moves_rook(board, selected_piece, piece[0])
+    elif piece[1] == "q":
+        return possible_moves_bishop(board, selected_piece, piece[0]) + possible_moves_rook(board, selected_piece, piece[0])
+    elif piece[1] == "k":
+        return possible_moves_king(board, selected_piece, piece[0])
+    
     return []
 
-def possible_moves_pawn(board, start_pos):
+def possible_moves_pawn(board, start_pos, color):
     x, y = start_pos
     possible = []
-    print(x, y)
+    print(x, y, color)
+
+    # set direction base on player
+    direction = -1 if color == "w" else 1
+    starting_row = 6 if color == "w" else 1
+    opponent_color = "b" if color == "w" else "w"
 
     # Moving one square forward
-    if x > 0 and board[x - 1][y] == "":
-        possible.append((x - 1, y))
+    if 0 <= x + direction < 8 and board[x + direction][y] == "":
+        possible.append((x + direction, y))
 
     # Moving two squares forward from the starting position
-    if x == 6 and board[x - 1][y] == "" and board[x - 2][y] == "":
-        possible.append((x - 2, y))
+    if x == starting_row and board[x + direction][y] == "" and board[x + 2 * direction][y] == "":
+        possible.append((x + 2 * direction, y))
 
     # Capture moves
-    if x > 0 and y > 0 and board[x - 1][y - 1] != "" and board[x - 1][y - 1][0] == "b":
-        possible.append((x - 1, y - 1))
-    if x > 0 and y < 7 and board[x - 1][y + 1] != "" and board[x - 1][y + 1][0] == "b":
-        possible.append((x - 1, y + 1))
+    if 0 <= x + direction < 8 and y > 0 and board[x + direction][y - 1] != "" and board[x + direction][y - 1][0] == opponent_color:
+        possible.append((x + direction, y - 1))
+    if 0 <= x + direction < 8 and y < 7 and board[x + direction][y + 1] != "" and board[x + direction][y + 1][0] == opponent_color:
+        possible.append((x + direction, y + 1))
     
     return possible
 
-def possible_moves_knight(board, start_pos):
+def possible_moves_knight(board, start_pos, color):
     x, y = start_pos
     possible = []
     print(x, y)
 
+    opponent_color = "b" if color == "w" else "w"
+
     # Up
     if x > 1:
-        if y > 0 and (board[x - 2][y - 1] == "" or board[x - 2][y - 1][0] == "b"):
+        if y > 0 and (board[x - 2][y - 1] == "" or board[x - 2][y - 1][0] == opponent_color):
             possible.append((x - 2, y - 1))
-        if y < 7 and (board[x - 2][y + 1] == "" or board[x - 2][y + 1][0] == "b"):
+        if y < 7 and (board[x - 2][y + 1] == "" or board[x - 2][y + 1][0] == opponent_color):
             possible.append((x - 2, y + 1))
 
     # Down
     if x < 6:
-        if y > 0 and (board[x + 2][y - 1] == "" or board[x + 2][y - 1][0] == "b"):
+        if y > 0 and (board[x + 2][y - 1] == "" or board[x + 2][y - 1][0] == opponent_color):
             possible.append((x + 2, y - 1))
-        if y < 7 and (board[x + 2][y + 1] == "" or board[x + 2][y + 1][0] == "b"):
+        if y < 7 and (board[x + 2][y + 1] == "" or board[x + 2][y + 1][0] == opponent_color):
             possible.append((x + 2, y + 1))
 
     # Left
     if y > 1:
-        if x > 0 and (board[x - 1][y - 2] == "" or board[x - 1][y - 2][0] == "b"):
+        if x > 0 and (board[x - 1][y - 2] == "" or board[x - 1][y - 2][0] == opponent_color):
             possible.append((x - 1, y - 2))
-        if x < 7 and (board[x + 1][y - 2] == "" or board[x + 1][y - 2][0] == "b"):
+        if x < 7 and (board[x + 1][y - 2] == "" or board[x + 1][y - 2][0] == opponent_color):
             possible.append((x + 1, y - 2))
 
     # Right
     if y < 6:
-        if x > 0 and (board[x - 1][y + 2] == "" or board[x - 1][y + 2][0] == "b"):
+        if x > 0 and (board[x - 1][y + 2] == "" or board[x - 1][y + 2][0] == opponent_color):
             possible.append((x - 1, y + 2))
-        if x < 7 and (board[x + 1][y + 2] == "" or board[x + 1][y + 2][0] == "b"):
+        if x < 7 and (board[x + 1][y + 2] == "" or board[x + 1][y + 2][0] == opponent_color):
             possible.append((x + 1, y + 2))
 
     return possible
 
-def possible_moves_bishop(board, start_pos):
+def possible_moves_bishop(board, start_pos, color):
     x, y = start_pos
     possible = []
     print(x, y)
+
+    opponent_color = "b" if color == "w" else "w"
 
     # Up-left
     i, j = x - 1, y - 1
     while i >= 0 and j >= 0:
         if board[i][j] == "":
             possible.append((i, j))
-        elif board[i][j][0] == "b":
+        elif board[i][j][0] == opponent_color:
             possible.append((i, j))
             break
         else:
@@ -138,7 +171,7 @@ def possible_moves_bishop(board, start_pos):
     while i >= 0 and j < 8:
         if board[i][j] == "":
             possible.append((i, j))
-        elif board[i][j][0] == "b":
+        elif board[i][j][0] == opponent_color:
             possible.append((i, j))
             break
         else:
@@ -151,7 +184,7 @@ def possible_moves_bishop(board, start_pos):
     while i < 8 and j >= 0:
         if board[i][j] == "":
             possible.append((i, j))
-        elif board[i][j][0] == "b":
+        elif board[i][j][0] == opponent_color:
             possible.append((i, j))
             break
         else:
@@ -164,7 +197,7 @@ def possible_moves_bishop(board, start_pos):
     while i < 8 and j < 8:
         if board[i][j] == "":
             possible.append((i, j))
-        elif board[i][j][0] == "b":
+        elif board[i][j][0] == opponent_color:
             possible.append((i, j))
             break
         else:
@@ -174,17 +207,19 @@ def possible_moves_bishop(board, start_pos):
 
     return possible
 
-def possible_moves_rook(board, start_pos):
+def possible_moves_rook(board, start_pos, color):
     x, y = start_pos
     possible = []
     print(x, y)
+
+    opponent_color = "b" if color == "w" else "w"
 
     # Up
     i = x - 1
     while i >= 0:
         if board[i][y] == "":
             possible.append((i, y))
-        elif board[i][y][0] == "b":
+        elif board[i][y][0] == opponent_color:
             possible.append((i, y))
             break
         else:
@@ -196,7 +231,7 @@ def possible_moves_rook(board, start_pos):
     while i < 8:
         if board[i][y] == "":
             possible.append((i, y))
-        elif board[i][y][0] == "b":
+        elif board[i][y][0] == opponent_color:
             possible.append((i, y))
             break
         else:
@@ -208,7 +243,7 @@ def possible_moves_rook(board, start_pos):
     while j >= 0:
         if board[x][j] == "":
             possible.append((x, j))
-        elif board[x][j][0] == "b":
+        elif board[x][j][0] == opponent_color:
             possible.append((x, j))
             break
         else:
@@ -220,7 +255,7 @@ def possible_moves_rook(board, start_pos):
     while j < 8:
         if board[x][j] == "":
             possible.append((x, j))
-        elif board[x][j][0] == "b":
+        elif board[x][j][0] == opponent_color:
             possible.append((x, j))
             break
         else:
@@ -229,40 +264,47 @@ def possible_moves_rook(board, start_pos):
 
     return possible
 
-def possible_moves_king(board, selected_piece):
+def possible_moves_king(board, selected_piece, color):
     x, y = selected_piece
     possible = []
     print(x, y)
 
+    opponent_color = "b" if color == "w" else "w"
+
     # Up
     if x > 0:
-        if board[x - 1][y] == "" or board[x - 1][y][0] == "b":
+        if board[x - 1][y] == "" or board[x - 1][y][0] == opponent_color:
             possible.append((x - 1, y))
-        if y > 0 and (board[x - 1][y - 1] == "" or board[x - 1][y - 1][0] == "b"):
+        if y > 0 and (board[x - 1][y - 1] == "" or board[x - 1][y - 1][0] == opponent_color):
             possible.append((x - 1, y - 1))
-        if y < 7 and (board[x - 1][y + 1] == "" or board[x - 1][y + 1][0] == "b"):
+        if y < 7 and (board[x - 1][y + 1] == "" or board[x - 1][y + 1][0] == opponent_color):
             possible.append((x - 1, y + 1))
 
     # Down
     if x < 7:
-        if board[x + 1][y] == "" or board[x + 1][y][0] == "b":
+        if board[x + 1][y] == "" or board[x + 1][y][0] == opponent_color:
             possible.append((x + 1, y))
-        if y > 0 and (board[x + 1][y - 1] == "" or board[x + 1][y - 1][0] == "b"):
+        if y > 0 and (board[x + 1][y - 1] == "" or board[x + 1][y - 1][0] == opponent_color):
             possible.append((x + 1, y - 1))
-        if y < 7 and (board[x + 1][y + 1] == "" or board[x + 1][y + 1][0] == "b"):
+        if y < 7 and (board[x + 1][y + 1] == "" or board[x + 1][y + 1][0] == opponent_color):
             possible.append((x + 1, y + 1))
 
     # Left
-    if y > 0 and (board[x][y - 1] == "" or board[x][y - 1][0] == "b"):
+    if y > 0 and (board[x][y - 1] == "" or board[x][y - 1][0] == opponent_color):
         possible.append((x, y - 1))
 
     # Right
-    if y < 7 and (board[x][y + 1] == "" or board[x][y + 1][0] == "b"):
+    if y < 7 and (board[x][y + 1] == "" or board[x][y + 1][0] == opponent_color):
         possible.append((x, y + 1))
     
+    # Castling
     if x == 7 and y == 4 and board[7][7] == "wr" and board[7][5] == "" and board[7][6] == "":
         possible.append((7, 6))
     elif x == 7 and y == 4 and board[7][0] == "wr" and board[7][1] == "" and board[7][2] == "" and board[7][3] == "":
         possible.append((7, 2))
+    elif x == 0 and y == 4 and board[0][7] == "br" and board[0][5] == "" and board[0][6] == "":
+        possible.append((0, 6))
+    elif x == 0 and y == 4 and board[0][0] == "br" and board[0][1] == "" and board[0][2] == "" and board[0][3] == "":
+        possible.append((0, 2))
 
     return possible
